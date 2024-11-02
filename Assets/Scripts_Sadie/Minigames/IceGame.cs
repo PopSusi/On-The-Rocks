8fa96842289ace467d668a7ced4134d8d8228328
makiset <mlh25080@gmail.com>
2024-11-02 17:32:57 -0400

    Ice game logic functional

--- Assets/Scripts_Sadie/Minigames/IceGame.cs
+++ Assets/Scripts_Sadie/Minigames/IceGame.cs
@@ -1,18 +1,78 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IceGame : MiniGameBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool perfectIce;
+    public float iceTarget;
+    public float iceCount;
+    public bool activeIce;
+
+    public void Start()
     {
+        Interaction();
+    }
+
+    public override void Interaction()
+    {
+        StartIce();
+
+    }
+
+    public override void ConfirmCheck()
+    {
+        if (perfectIce)
+        {
+            Debug.Log("Success!");
+        }
+        else
+        {
+            Debug.Log("Fail");
+        }
+        activeIce = false;
+    }
+
+    public void StartIce()
+    {
+        activeIce = true;
+        iceTarget = Random.Range(10, 31);
+        Debug.Log(iceTarget);
+        iceCount = 0;
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("RAH");
+        if (activeIce)
+        {
+            iceCount++;
+            Debug.Log(iceCount);
+            checkIce();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void checkIce()
     {
-        
+        if (iceCount == iceTarget)
+        {
+            perfectIce = true;
+            StartCoroutine(waiter());
+            
+
+        }
     }
-}
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);
+        if (iceCount != iceTarget)
+        {
+            perfectIce = false;
+        }
+        ConfirmCheck();
+    }
+
+    }
