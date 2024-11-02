9c8222102dbbb1e2ff8b6479acead06d418664ed
Susi <braeden8.susi6@gmail.com>
2024-11-02 11:32:55 -0400

    Event for Load Minigame

--- Assets/SusiScripts/Page.cs
+++ Assets/SusiScripts/Page.cs
@@ -6,18 +6,25 @@ using UnityEngine.UI;
 
 public class Page : MonoBehaviour
 {
+    public delegate void IngredientSend(IngredientBase ingredient);
+    public static event IngredientSend Send;
+
     public TextMeshProUGUI[] body;
     public TextMeshProUGUI[] head;
     public Image[] icon;
-    public IngredientBase[] ingredient;
+    public IngredientBase[] ingredients;
 
     public void SetValues()
     {
-        //body.text[0] = ingredient[0].bookText; edit Scriptable Object to be string
-        //head[0].text = ingredient[0].type.ToString();
-        icon[0].sprite = ingredient[0].bookSprite;
-        //body.text[1] = ingredient[1].bookText; edit Scriptable Object to be string
-        //head[1].text = ingredient[1].type.ToString();
-        icon[1].sprite = ingredient[1].bookSprite;
+        //body.text[0] = ingredients[0].bookText; edit Scriptable Object to be string
+        //head[0].text = ingredients[0].type.ToString();
+        icon[0].sprite = ingredients[0].bookSprite;
+        //body.text[1] = ingredients[1].bookText; edit Scriptable Object to be string
+        //head[1].text = ingredients[1].type.ToString();
+        icon[1].sprite = ingredients[1].bookSprite;
+    }
+    public void SendEvent(int index)
+    {
+        Send(ingredients[index]);
     }
 }
